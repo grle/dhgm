38512be871ae847d2e5aa344b07f23921dcc9c42
Grace Lee <graceleenexus@gmail.com>
2022-10-06 16:13:22 -0700

    site nav update

--- js/script.js
+++ js/script.js
@@ -4,3 +4,22 @@
 //     this.classList.toggle("is-active");
 //   });
 // });
+
+
+let boolMenu = false;
+let mobile = document.getElementsByClassName("mobile-menu");
+let bg = document.getElementById("backdrop");
+
+function toggleMenu() {
+  let ham = document.getElementById("hamburger-1");
+  ham.classList.toggle("is-active");
+
+  if (boolMenu == false) {
+    mobile[0].classList.add("visibility");
+    boolMenu = true;
+  }
+  else if (boolMenu == true) {
+    mobile[0].classList.remove("visibility");
+    boolMenu = false;
+  }
+}
